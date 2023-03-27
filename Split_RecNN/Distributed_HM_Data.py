6ddd2010a7624d4010ac20370a3e9f430739aa9a
Haoyuan-L <haoyuan.li.9654@gmail.com>
2023-03-27 17:58:59 +0200

    create Utils scripts for reused class and methods

--- Split_RecNN/Distributed_HM_Data.py
+++ Split_RecNN/Distributed_HM_Data.py
@@ -1,4 +1,77 @@
 import torch
+from torch.utils.data import Dataset
+from tqdm.notebook import tqdm
+import numpy as np
+
+class HMSaleTrainDataLoader(Dataset):
+    """HMSaleTrainDataLoader Training set of HM sales data
+
+    Args:
+        transactions (pd.DataFrame): Dataframe of transaction records
+        all_products_id (list): A list contains all product ids
+    """
+    def __init__(self, transactions, all_products_id):
+        self.customers, self.products, self.prices, self.sales_channels, \
+        self.club_status, self.age_groups, self.product_groups, self.color_groups, \
+        self.index_name, self.labels = self.get_dataset(transactions, all_products_id)
+
+    def __len__(self):
+        return len(self.customers)
+    
+    def __getitem__(self, idx):
+        return self.customers[idx], self.products[idx], self.prices[idx], self.sales_channels[idx], self.club_status[idx], \
+               self.age_groups[idx], self.product_groups[idx], self.color_groups[idx], self.index_name[idx], self.labels[idx]
+    
+    def get_dataset(self, transactions, all_products_id):
+        customers, products, prices, sales_channels, club_status, age_groups, product_groups, color_groups, index_name, labels  = [], [], [], [], [], [], [], [], [], []
+        customer_product_set = set(zip(transactions["customer_id"], transactions["article_id"], 
+                                       transactions["price"], transactions["sales_channel_id"], 
+                                       transactions["club_member_status"], transactions["age"], 
+                                       transactions["product_group_name"], transactions["colour_group_name"], transactions["index_name"]))
+        
+        """negative sampling"""
+        # set up negative:positive ratio as 4:1
+        negative_samples = 4
+
+        for u, i, price, sale, club, age, product, color, index in tqdm(customer_product_set):
+            customers.append(u)
+            products.append(i)
+            prices.append(price)
+            sales_channels.append(sale)
+            club_status.append(club)
+            age_groups.append(age)
+            product_groups.append(product)
+            color_groups.append(color)
+            index_name.append(index)
+            labels.append(1)
+            for _ in range(negative_samples):
+                negative_product = np.random.choice(all_products_id)
+                while (u, negative_product, price, sale, club, age, product, color, index) in customer_product_set:
+                    negative_product = np.random.choice(all_products_id)
+                customers.append(u)
+                products.append(negative_product)
+                prices.append(price)
+                sales_channels.append(sale)
+                club_status.append(club)
+                age_groups.append(age)
+                product_groups.append(product)
+                color_groups.append(color)
+                index_name.append(index)
+                labels.append(0)
+        
+        customers = torch.tensor(customers)
+        products = torch.tensor(products)
+        prices = torch.tensor(prices)
+        sales_channels = torch.tensor(sales_channels)
+        club_status = torch.tensor(club_status)
+        age_groups = torch.tensor(age_groups)
+        product_groups = torch.tensor(product_groups)
+        color_groups = torch.tensor(color_groups)
+        index_name = torch.tensor(index_name)
+        labels = torch.tensor(labels)
+        
+        return customers, products, prices, sales_channels, club_status, age_groups, product_groups, color_groups, index_name, labels
+
 
 class Distributed_HM:
     def __init__(self, data_owners, data_loader):
---
+++ Split_RecNN/utils_models.py
@@ -0,0 +1,149 @@
+import torch
+from torch import nn
+
+class SalesNN(nn.Module):
+    """ Partial model for sales domain
+    Args:
+        num_users (int): Number of users
+        num_items (int): Number of products
+        prices (float): price of transactions
+        sales_channels (float): sales channels
+    
+    """
+    def __init__(
+            self, 
+            num_users: int, 
+            num_items: int,
+            input_size: int = 50,
+            user_embedding_dim: int = 16,
+            item_embedding_dim: int = 32,
+            hidden_size_1: int = 128,
+            output_size: int = 32,
+        ):
+        super().__init__()
+        self.user_embedding_layer = nn.Embedding(num_embeddings=num_users, embedding_dim=user_embedding_dim)
+        self.item_embedding_layer = nn.Embedding(num_embeddings=num_items, embedding_dim=item_embedding_dim)
+        self.relu = nn.LeakyReLU()
+        if num_users and num_items is not None:
+                in_channels = (
+                    [input_size] 
+                    + [hidden_size_1]
+                    + [output_size]
+                )
+        else:
+            raise ValueError
+        self.encoder = nn.Sequential(
+            *[nn.Linear(in_features=in_channels[i], out_features=in_channels[i+1]) for i in range(len(in_channels)-1) if i != len(in_channels)-1]
+        )
+        
+    def forward(self, user_input, item_input, prices, sales_channels):
+        user_embedding = self.user_embedding_layer(user_input)
+        item_embedding = self.item_embedding_layer(item_input)
+#         user_embedding = torch.squeeze(user_embedding, dim=1)
+#         item_embedding = torch.squeeze(item_embedding, dim=1)
+        
+        latent_vec = torch.cat([user_embedding, item_embedding, prices, sales_channels], dim=-1)
+        for layer in self.encoder:
+            latent_vec = layer(latent_vec)
+            latent_vec = self.relu(latent_vec)
+        
+        return latent_vec
+    
+    # save weights of partial model on remote worker
+    def get_weights(self):
+        return self.state_dict()
+    
+class CustomersNN(nn.Module):
+    """ Partial model for customer domain
+    Args:
+        club_status (int): active or inactive customers' status
+        age_groups (int): age of customers
+    
+    """
+    def __init__(
+            self,
+            input_size: int = 2,
+            output_size: int = 5,
+        ):
+        super().__init__()
+        self.relu = nn.LeakyReLU()
+        in_channels = (
+            [input_size] 
+            + [output_size]
+        )
+        
+        self.encoder = nn.Sequential(
+            *[nn.Linear(in_features=in_channels[i], out_features=in_channels[i+1]) for i in range(len(in_channels)-1) if i != len(in_channels)-1]
+        )
+        
+    def forward(self, club_status, age_groups):
+        
+        latent_vec = torch.cat([club_status, age_groups], dim=-1)
+        
+        for layer in self.encoder:
+            latent_vec = layer(latent_vec)
+            latent_vec = self.relu(latent_vec)
+        
+        return latent_vec
+    
+    # save weights of partial model on remote worker
+    def get_weights(self):
+        return self.state_dict()
+
+class ProductsNN(nn.Module):
+    """ Partial model for product domain
+    Args:
+        num_product_groups (int): Number of product groups
+        num_color_groups: (int): Number of color groups
+        num_index_name: (int): Number of index name
+    
+    """
+    def __init__(
+            self,
+            num_product_groups: int,
+            num_color_groups: int,
+            num_index_name: int,
+            product_group_embedding_dim: int = 8,
+            color_group_embedding_dim: int = 16,
+            index_name_embedding_dim: int = 6,
+            input_size: int = 30,
+            hidden_size_1: int = 64,
+            output_size: int = 16,
+        ):
+        super().__init__()
+        self.product_group_embedding_layer = nn.Embedding(num_embeddings=num_product_groups, embedding_dim=product_group_embedding_dim)
+        self.color_group_embedding_layer = nn.Embedding(num_embeddings=num_color_groups, embedding_dim=color_group_embedding_dim)
+        self.index_name_embedding_layer = nn.Embedding(num_embeddings=num_index_name, embedding_dim=index_name_embedding_dim)
+        self.relu = nn.LeakyReLU()
+        
+        if num_product_groups and num_color_groups and num_index_name is not None:
+                in_channels = (
+                    [input_size] 
+                    + [hidden_size_1]
+                    + [output_size]
+                )
+        else:
+            raise ValueError
+        self.encoder = nn.Sequential(
+            *[nn.Linear(in_features=in_channels[i], out_features=in_channels[i+1]) for i in range(len(in_channels)-1) if i != len(in_channels)-1]
+        )
+        
+    def forward(self, product_groups, color_groups, index_name):
+        product_group_embedding = self.product_group_embedding_layer(product_groups)
+        color_group_embedding = self.color_group_embedding_layer(color_groups)
+        index_name_embedding = self.index_name_embedding_layer(index_name)
+#         product_group_embedding = torch.squeeze(product_group_embedding, dim=1)
+#         color_group_embedding = torch.squeeze(color_group_embedding, dim=1)
+#         index_name_embedding = torch.squeeze(index_name_embedding, dim=1)
+        
+        latent_vec = torch.cat([product_group_embedding, color_group_embedding, index_name_embedding], dim=-1)
+        
+        for layer in self.encoder:
+            latent_vec = layer(latent_vec)
+            latent_vec = self.relu(latent_vec)
+        
+        return latent_vec
+    
+    # save weights of partial model on remote worker
+    def get_weights(self):
+        return self.state_dict()
