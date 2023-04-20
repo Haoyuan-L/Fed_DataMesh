be0aa07e011d02113f5eb24688e9996379b0952f
Haoyuan-L <haoyuan.li.9654@gmail.com>
2023-04-21 01:53:04 +0200

    rewrite the domain models

--- Split_RecNN/utils_models.py
+++ Split_RecNN/utils_models.py
@@ -14,39 +14,22 @@ class SalesNN(nn.Module):
             self, 
             num_users: int, 
             num_items: int,
-            input_size: int = 50,
-            user_embedding_dim: int = 16,
-            item_embedding_dim: int = 32,
+            input_size: int = 48,
+            user_embedding_dim: int = 32,
+            item_embedding_dim: int = 64,
             hidden_size_1: int = 128,
             output_size: int = 32,
         ):
         super().__init__()
         self.user_embedding_layer = nn.Embedding(num_embeddings=num_users, embedding_dim=user_embedding_dim)
         self.item_embedding_layer = nn.Embedding(num_embeddings=num_items, embedding_dim=item_embedding_dim)
-        self.relu = nn.LeakyReLU()
-        if num_users and num_items is not None:
-                in_channels = (
-                    [input_size] 
-                    + [hidden_size_1]
-                    + [output_size]
-                )
-        else:
-            raise ValueError
-#         self.encoder = nn.Sequential(
-#             *[nn.Linear(in_features=in_channels[i], out_features=in_channels[i+1]) for i in range(len(in_channels)-1) if i != len(in_channels)-1]
-#         )
         
-    def forward(self, user_input, item_input, prices, sales_channels):
+    def forward(self, user_input, item_input):
         user_embedding = self.user_embedding_layer(user_input)
         item_embedding = self.item_embedding_layer(item_input)
-#         user_embedding = torch.squeeze(user_embedding, dim=1)
-#         item_embedding = torch.squeeze(item_embedding, dim=1)
-        
-        latent_vec = torch.cat([user_embedding, item_embedding, prices, sales_channels], dim=-1)
-#         for layer in self.encoder:
-#             latent_vec = layer(latent_vec)
-#             latent_vec = self.relu(latent_vec)
-        
+
+        latent_vec = torch.cat([user_embedding, item_embedding], dim=-1)
+
         return latent_vec
     
     # save weights of partial model on remote worker
@@ -82,7 +65,6 @@ class CustomersNN(nn.Module):
         
         for layer in self.encoder:
             latent_vec = layer(latent_vec)
-            latent_vec = self.relu(latent_vec)
         
         return latent_vec
     
@@ -106,41 +88,20 @@ class ProductsNN(nn.Module):
             product_group_embedding_dim: int = 8,
             color_group_embedding_dim: int = 16,
             index_name_embedding_dim: int = 6,
-            input_size: int = 30,
-            hidden_size_1: int = 64,
-            output_size: int = 16,
         ):
         super().__init__()
         self.product_group_embedding_layer = nn.Embedding(num_embeddings=num_product_groups, embedding_dim=product_group_embedding_dim)
         self.color_group_embedding_layer = nn.Embedding(num_embeddings=num_color_groups, embedding_dim=color_group_embedding_dim)
         self.index_name_embedding_layer = nn.Embedding(num_embeddings=num_index_name, embedding_dim=index_name_embedding_dim)
-        self.relu = nn.LeakyReLU()
         
-        if num_product_groups and num_color_groups and num_index_name is not None:
-                in_channels = (
-                    [input_size] 
-                    + [hidden_size_1]
-                    + [output_size]
-                )
-        else:
-            raise ValueError
-#         self.encoder = nn.Sequential(
-#             *[nn.Linear(in_features=in_channels[i], out_features=in_channels[i+1]) for i in range(len(in_channels)-1) if i != len(in_channels)-1]
-#         )
+
         
     def forward(self, product_groups, color_groups, index_name):
         product_group_embedding = self.product_group_embedding_layer(product_groups)
         color_group_embedding = self.color_group_embedding_layer(color_groups)
         index_name_embedding = self.index_name_embedding_layer(index_name)
-#         product_group_embedding = torch.squeeze(product_group_embedding, dim=1)
-#         color_group_embedding = torch.squeeze(color_group_embedding, dim=1)
-#         index_name_embedding = torch.squeeze(index_name_embedding, dim=1)
-        
+
         latent_vec = torch.cat([product_group_embedding, color_group_embedding, index_name_embedding], dim=-1)
-        
-#         for layer in self.encoder:
-#             latent_vec = layer(latent_vec)
-#             latent_vec = self.relu(latent_vec)
         
         return latent_vec
     
